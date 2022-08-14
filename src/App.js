ab378f5f1f2b6422eb5270d9672292a027be97d2
Mahan Jafari <mahanjafari14@gmail.com>
2022-08-14 16:14:00 +0430

    Functions done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
-import "./App.css";
+import React, { useState } from "react";
 import Square from "./components/Square/Square";
+import "./App.css";
 
 const App = () => {
+  const [state, setState] = useState({
+    player: "circle",
+    positions: [
+      "empty",
+      "empty",
+      "empty",
+      "empty",
+      "empty",
+      "empty",
+      "empty",
+      "empty",
+      "empty",
+    ],
+  });
+
+  const takeTurn = (position) => {
+    const positions = [...state.positions];
+    positions[position] = state.player;
+
+    setState(
+      { player: state.player == "circle" ? "cross" : "circle" },
+      positions
+    );
+  };
+
   return (
     <div className="app">
       <div className="grid">
-        <Square position={0} />
-        <Square position={1} />
-        <Square position={2} />
-        <Square position={3} />
-        <Square position={4} />
-        <Square position={5} />
-        <Square position={6} />
-        <Square position={7} />
-        <Square position={8} />
+        <Square position={0} value={state.positions[0]} takeTurn={takeTurn} />
+        <Square position={1} value={state.positions[1]} takeTurn={takeTurn} />
+        <Square position={2} value={state.positions[2]} takeTurn={takeTurn} />
+        <Square position={3} value={state.positions[3]} takeTurn={takeTurn} />
+        <Square position={4} value={state.positions[4]} takeTurn={takeTurn} />
+        <Square position={5} value={state.positions[5]} takeTurn={takeTurn} />
+        <Square position={6} value={state.positions[6]} takeTurn={takeTurn} />
+        <Square position={7} value={state.positions[7]} takeTurn={takeTurn} />
+        <Square position={8} value={state.positions[8]} takeTurn={takeTurn} />
       </div>
     </div>
   );
--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -2,10 +2,15 @@ import Circle from "../Circle/Circle";
 import Cross from "../Cross/Cross";
 import "./Square.css";
 
-const Square = ({ position }) => {
+const Square = ({ position, value, takeTurn }) => {
+  const handleClick = () => {
+    if (value == "empty") takeTurn(position);
+  };
+
   return (
-    <div className="square">
-      <Cross />
+    <div className="square" onClick={handleClick}>
+      {value == "circle" && <Circle />}
+      {value == "cross" && <Cross />}
     </div>
   );
 };
